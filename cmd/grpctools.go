03114b93348b1f63c1eb2889dfdad3e4280fb217
张重阳 <zcy89@live.cn>
2019-12-26 12:41:52 +0800

    mm

--- cmd/grpctools.go
+++ cmd/grpctools.go
@@ -18,10 +18,11 @@ func main() {
 	pflag.StringP("dir", "d", "", "proto文件夹根目录")
 	pflag.StringP("out", "o", "", "DAO文件输出路径")
 	pflag.StringP("language", "l", "", "语言: java, cs, js, kotlin, swift, oc")
+	pflag.StringP("suffix", "s", "", "命名空间后缀")
 	pflag.BoolP("help", "h", false, "参数说明")
 	pflag.Parse()
 	viper.BindPFlags(pflag.CommandLine)
-	if len(os.Args) == 2 && (os.Args[1] == "help" || os.Args[1] == "--help" || os.Args[1] == "-h" ) {
+	if len(os.Args) == 2 && (os.Args[1] == "help" || os.Args[1] == "--help" || os.Args[1] == "-h") {
 		pflag.Usage()
 		return
 	}
@@ -40,7 +41,7 @@ func main() {
 	}
 	//files := []string{}
 	fileDescs := []*desc.FileDescriptor{}
-	parser := protoparse.Parser{IncludeSourceCodeInfo:true, ImportPaths: []string{viper.GetString("dir")}}
+	parser := protoparse.Parser{IncludeSourceCodeInfo: true, ImportPaths: []string{viper.GetString("dir")}}
 	filepath.Walk(viper.GetString("dir"), func(path string, info os.FileInfo, err error) error {
 		if !info.IsDir() && strings.HasSuffix(info.Name(), ".proto") {
 			relPath, err := filepath.Rel(viper.GetString("dir"), path)
@@ -48,7 +49,7 @@ func main() {
 				fmt.Println(err)
 			}
 			d, err := parser.ParseFiles(relPath)
-			if err != nil{
+			if err != nil {
 				fmt.Println(err)
 				fmt.Println("解析proto文件出错。可能是因为文本编码问题，windows上应为GB2312. visual studio中打开高级保存选项，设置文件为GB2312代码页936.")
 				return nil
@@ -62,30 +63,30 @@ func main() {
 	})
 
 	outputs := map[string]*desc.FileDescriptor{}
-	for _, fileDesc := range fileDescs{
+	for _, fileDesc := range fileDescs {
 		rootDir := viper.GetString("out")
 		paths := strings.Split(strings.TrimSuffix(fileDesc.GetName(), ".proto"), string(os.PathSeparator))
-		dir := filepath.Join(rootDir, filepath.Join(paths[0:len(paths) - 1]...))
+		dir := filepath.Join(rootDir, filepath.Join(paths[0:len(paths)-1]...))
 		os.MkdirAll(dir, os.ModeDir)
-		fullPath := filepath.Join(dir, string(os.PathSeparator), paths[len(paths) - 1] + ".cs")
+		fullPath := filepath.Join(dir, string(os.PathSeparator), paths[len(paths)-1]+".cs")
 		os.Create(fullPath)
 		outputs[fullPath] = fileDesc
 	}
 	metas := pkg.GenerateCSharp(outputs, viper.GetString("language"))
 	//temp, err := template.ParseFiles("./template/cs.gohtml")
 	ptemp, err := pongo2.FromFile("./template/cs.temp")
-	if err != nil{
+	if err != nil {
 		fmt.Println(err)
 	}
-	for _, fileArg := range metas{
+	for _, fileArg := range metas {
 		os.Remove(fileArg.FullFileName)
 		fi, err := os.Create(fileArg.FullFileName)
-		if err != nil{
+		if err != nil {
 			log.Panic(err)
 			return
 		}
 		//temp.Execute(fi, fileArg)
-		ptemp.Options.Update(&pongo2.Options{TrimBlocks:true, LStripBlocks:true})
+		ptemp.Options.Update(&pongo2.Options{TrimBlocks: true, LStripBlocks: true})
 		ptemp.ExecuteWriter(pongo2.Context{"data": fileArg}, fi)
 		fi.Close()
 	}
--- pkg/ProtoParser.go
+++ pkg/ProtoParser.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/golang/protobuf/protoc-gen-go/descriptor"
 	"github.com/jhump/protoreflect/desc"
+	"github.com/spf13/viper"
 	"log"
 	"path/filepath"
 	"strings"
@@ -12,6 +13,7 @@ import (
 type File struct {
 	FileName     string
 	FullFileName string
+	Suffix       string
 	Messages     []*Message
 	CsNamespace  string
 	JavaPackage  string
@@ -22,6 +24,7 @@ type File struct {
 
 type Message struct {
 	Name         string
+	Suffix       string
 	csNamespace  string
 	javaPackage  string
 	phpNamespace string
@@ -34,20 +37,22 @@ type Message struct {
 type FieldType int
 
 const (
-	FieldType_Message = 0
-	FieldType_Value = 1
-	FieldType_Map = 2
+	FieldType_Message  = 0
+	FieldType_Value    = 1
+	FieldType_Map      = 2
 	FieldType_Repeated = 3
 )
 
 type Field struct {
-	Name      string
-	CsType    string
-	JavaType  string
-	GoType    string
-	PhpType   string
-	SwiftType string
-	ObjcType  string
+	Name        string
+	WrapperType string
+	Number      int32
+	CsType      string
+	JavaType    string
+	GoType      string
+	PhpType     string
+	SwiftType   string
+	ObjcType    string
 
 	CsKeyType    string
 	JavaKeyType  string
@@ -56,24 +61,32 @@ type Field struct {
 	SwiftKeyType string
 	ObjcKeyType  string
 
+	CsMessageType    string
+	JavaMessageType  string
+	GoMessageType    string
+	PhpMessageType   string
+	SwiftMessageType string
+	ObjcMessageType  string
+
 	FieldType FieldType
 	Comment   []string
-	Nullable	bool
+	Nullable  bool
 }
 
 func GenerateCSharp(files map[string]*desc.FileDescriptor, language string) []*File {
 	result := []*File{}
 	fqns := map[string]*Message{}
-	for file, fileDesc := range files{
+	for file, fileDesc := range files {
 		file := &File{
 			FileName:     filepath.Base(file),
 			FullFileName: file,
+			Suffix:       viper.GetString("suffix"),
 			Messages:     []*Message{},
 			CsNamespace:  fileDesc.GetFileOptions().GetCsharpNamespace(),
 			JavaPackage:  fileDesc.GetFileOptions().GetJavaPackage(),
 			PhpNamespace: fileDesc.GetFileOptions().GetPhpNamespace(),
 			GoPackage:    fileDesc.GetFileOptions().GetGoPackage(),
-			Comment:	  getComment(fileDesc.GetSourceInfo()),
+			Comment:      getComment(fileDesc.GetSourceInfo()),
 		}
 		if file.CsNamespace == "" {
 			file.CsNamespace = fileDesc.GetPackage()
@@ -88,53 +101,59 @@ func GenerateCSharp(files map[string]*desc.FileDescriptor, language string) []*F
 			file.GoPackage = fileDesc.GetPackage()
 		}
 
-		for _, messageDesc := range fileDesc.GetMessageTypes(){
+		for _, messageDesc := range fileDesc.GetMessageTypes() {
 			msg := &Message{
 				Name:         messageDesc.GetName(),
 				Fields:       []*Field{},
+				Suffix:       viper.GetString("suffix"),
 				desc:         messageDesc,
 				csNamespace:  file.CsNamespace,
 				javaPackage:  file.JavaPackage,
 				phpNamespace: file.PhpNamespace,
 				goPackage:    file.GoPackage,
-				Comment:	  getComment(messageDesc.GetSourceInfo()),
+				Comment:      getComment(messageDesc.GetSourceInfo()),
 			}
 			file.Messages = append(file.Messages, msg)
 			fqns[messageDesc.GetFullyQualifiedName()] = msg
 		}
 		result = append(result, file)
 	}
-	for _, file := range result{
+	for _, file := range result {
 		for _, msg := range file.Messages {
-			for _, fieldDesc := range msg.desc.GetFields(){
+			for _, fieldDesc := range msg.desc.GetFields() {
 				field := &Field{
-					Name: fieldDesc.GetName(),
-					Comment:	  getComment(fieldDesc.GetSourceInfo()),
+					Name:    fieldDesc.GetName(),
+					Number:  fieldDesc.GetNumber(),
+					Comment: getComment(fieldDesc.GetSourceInfo()),
 				}
 				if fieldDesc.IsMap() {
 					switch language {
 					case "cs":
-						field.CsKeyType = getFieldTypeName(fieldDesc.GetMapKeyType(), fqns, language)
-						field.CsType = getFieldTypeName(fieldDesc.GetMapValueType(), fqns, language)
+						field.CsKeyType, _ = getFieldTypeName(fieldDesc.GetMapKeyType(), fqns, language)
+						field.CsType, field.CsMessageType = getFieldTypeName(fieldDesc.GetMapValueType(), fqns, language)
+						if fieldDesc.GetMessageType() != nil && fieldDesc.GetMessageType().GetFile().GetPackage() == "com.google.protobuf" {
+							field.Nullable = true
+						}
 					}
 					field.FieldType = FieldType_Map
-				}else{
+				} else {
 					if fieldDesc.IsRepeated() {
 						field.FieldType = FieldType_Repeated
-					}else if fieldDesc.GetMessageType()!=nil{
+					} else if fieldDesc.GetMessageType() != nil {
 						if fieldDesc.GetMessageType().GetFile().GetPackage() == "com.google.protobuf" {
 							field.FieldType = FieldType_Value
 							field.Nullable = true
-						}else {
+							field.WrapperType = fieldDesc.GetMessageType().GetName()
+						} else {
 							field.FieldType = FieldType_Message
 						}
-					}else{
+					} else {
 						field.FieldType = FieldType_Value
 					}
 
 					switch language {
 					case "cs":
-						field.CsType = getFieldTypeName(fieldDesc, fqns, language)
+						field.CsType, field.CsMessageType = getFieldTypeName(fieldDesc, fqns, language)
 					}
 				}
 				msg.Fields = append(msg.Fields, field)
@@ -145,37 +164,42 @@ func GenerateCSharp(files map[string]*desc.FileDescriptor, language string) []*F
 }
 
 var typeMap = map[descriptor.FieldDescriptorProto_Type]map[string]string{
-	descriptor.FieldDescriptorProto_TYPE_BOOL: {"cs": "bool"},
-	descriptor.FieldDescriptorProto_TYPE_BYTES: {"cs": "byte[]"},
+	descriptor.FieldDescriptorProto_TYPE_BOOL:   {"cs": "bool"},
+	descriptor.FieldDescriptorProto_TYPE_BYTES:  {"cs": "byte[]"},
 	descriptor.FieldDescriptorProto_TYPE_DOUBLE: {"cs": "double"},
-	descriptor.FieldDescriptorProto_TYPE_FLOAT: {"cs": "float"},
-	descriptor.FieldDescriptorProto_TYPE_INT32: {"cs": "int32"},
-	descriptor.FieldDescriptorProto_TYPE_INT64: {"cs": "int64"},
+	descriptor.FieldDescriptorProto_TYPE_FLOAT:  {"cs": "float"},
+	descriptor.FieldDescriptorProto_TYPE_INT32:  {"cs": "int"},
+	descriptor.FieldDescriptorProto_TYPE_INT64:  {"cs": "long"},
 	descriptor.FieldDescriptorProto_TYPE_STRING: {"cs": "string"},
 }
 
-func getFieldTypeName(fieldDesc *desc.FieldDescriptor, fqns map[string]*Message, language string)string{
-	if fieldDesc.GetMessageType() != nil{
-		if fieldDesc.GetMessageType().GetFile().GetPackage() == "com.google.protobuf"{
-			return typeMap[fieldDesc.GetMessageType().GetFields()[0].GetType()][language]
-		}else {
+func getFieldTypeName(fieldDesc *desc.FieldDescriptor, fqns map[string]*Message, language string) (string, string) {
+	if fieldDesc.GetMessageType() != nil {
+		if fieldDesc.GetMessageType().GetFile().GetPackage() == "com.google.protobuf" {
+			return typeMap[fieldDesc.GetMessageType().GetFields()[0].GetType()][language], typeMap[fieldDesc.GetMessageType().GetFields()[0].GetType()][language]
+		} else {
 			msg := fqns[fieldDesc.GetMessageType().GetFullyQualifiedName()]
 			switch language {
 			case "cs":
-				return fmt.Sprintf("%s.%s", msg.csNamespace, msg.Name)
+				result := msg.csNamespace
+				if len(msg.Suffix) > 0 {
+					result += "." + msg.Suffix
+				}
+				result += "." + msg.Name
+				return result, fmt.Sprintf("%s.%s", msg.csNamespace, msg.Name)
 			}
 		}
-	}else{
-		return typeMap[fieldDesc.GetType()][language]
+	} else {
+		return typeMap[fieldDesc.GetType()][language], typeMap[fieldDesc.GetType()][language]
 	}
 	log.Fatal("unsupport field type", fieldDesc.GetType())
-	return ""
+	return "", ""
 }
 
-func getComment(si *descriptor.SourceCodeInfo_Location)[]string{
+func getComment(si *descriptor.SourceCodeInfo_Location) []string {
 	result := []string{}
 	cms := strings.Split(strings.Trim(strings.ReplaceAll(si.GetLeadingComments(), "\r\n", "\n"), "\n"), "\n")
-	for _, item := range cms{
+	for _, item := range cms {
 		result = append(result, item)
 	}
 	result = append(result, strings.Trim(strings.ReplaceAll(si.GetTrailingComments(), "\r\n", "\n"), "\n"))
